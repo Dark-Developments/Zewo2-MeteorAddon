24a31ec24f45d153b00877463274219ff3c18207
Antho <anthony.ashworth007@gmail.com>
2023-04-23 15:21:37 +0100

    Made antispawnpoint actually work

--- src/main/java/com/nxyi/addon/modules/AntiSpawnpoint.java
+++ src/main/java/com/nxyi/addon/modules/AntiSpawnpoint.java
@@ -2,15 +2,34 @@ package com.nxyi.addon.modules;
 
 import com.nxyi.addon.Addon;
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.DoubleSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.block.BedBlock;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.RespawnAnchorBlock;
+import net.minecraft.network.packet.c2s.handshake.HandshakeC2SPacket;
+import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
 import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 
 public class AntiSpawnpoint extends Module {
+
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Boolean> fakeuse = sgGeneral.add(new BoolSetting.Builder()
+        .name("Fake Use")
+        .description("Makes it look like you interacted with the block.")
+        .defaultValue(false)
+        .build()
+    );
     public AntiSpawnpoint() {
         super(Addon.CATEGORY, "AntiSpawnpoint", "Stop you from setting your spawnpoint");
     }
@@ -18,7 +37,15 @@ public class AntiSpawnpoint extends Module {
     @EventHandler
     private void onclick(PacketEvent.Send event){
         if (event.packet instanceof PlayerInteractBlockC2SPacket packet) {
-            if (mc.world.getBlockState(packet.getBlockHitResult().getBlockPos()).getBlock().getName().contains(Text.of("Bed"))) {
+            BlockPos blockpos = packet.getBlockHitResult().getBlockPos();
+
+            if (fakeuse.get()){
+                mc.player.networkHandler.sendPacket(new HandSwingC2SPacket(Hand.MAIN_HAND));
+            }
+
+            if (mc.world.getDimension().bedWorks() && mc.world.getBlockState(blockpos).getBlock() instanceof BedBlock) {
+                event.cancel();
+            } else if (mc.world.getDimension().respawnAnchorWorks() && mc.world.getBlockState(blockpos).getBlock() instanceof RespawnAnchorBlock) {
                 event.cancel();
             }
         }
