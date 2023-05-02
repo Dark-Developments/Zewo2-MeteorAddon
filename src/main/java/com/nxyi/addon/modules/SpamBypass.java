1f5a5333a9b8001704b3b06e24f815d47fb878c6
Antho <anthony.ashworth007@gmail.com>
2023-05-02 20:47:26 +0100

    added rndString to spambypass

--- src/main/java/com/nxyi/addon/modules/SpamBypass.java
+++ src/main/java/com/nxyi/addon/modules/SpamBypass.java
@@ -1,18 +1,27 @@
 package com.nxyi.addon.modules;
 
 import com.nxyi.addon.Addon;
+import com.nxyi.addon.Utils.JinxUtils;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.orbit.EventHandler;
 
 public class SpamBypass extends Module {
 
     private final SettingGroup sgGeneral = this.settings.getDefaultGroup();
 
-    private final Setting<String> bc = sgGeneral.add(new StringSetting.Builder().name("Command").description("Bypass Command").defaultValue("/skill").build());
 
     private final Setting<String> message = sgGeneral.add(new StringSetting.Builder().name("Message").description("Message To Spam").defaultValue("Jinx").build());
+
+    private final Setting<Bypass> bypass = sgGeneral.add(new EnumSetting.Builder<Bypass>()
+        .name("bypass")
+        .description("bypass")
+        .defaultValue(Bypass.Command)
+        .build()
+    );
+    private final Setting<String> bc = sgGeneral.add(new StringSetting.Builder().name("Command").description("Bypass Command").defaultValue("/skill").visible(() -> bypass.get().equals(Bypass.Command)).build());
 
     private final Setting<Modes> mode = sgGeneral.add(new EnumSetting.Builder<Modes>()
         .name("mode")
@@ -57,7 +66,7 @@ public class SpamBypass extends Module {
 
             case DelayPerTick -> {
                 if (timer <= 0) {
-                    mc.player.networkHandler.sendChatMessage(bc + " " + message);
+                    mc.player.networkHandler.sendChatMessage(((bypass.get() == Bypass.Command) ? bc.get() : "") + " " + message + (((bypass.get() == Bypass.RndString) ? " " + JinxUtils.randomString(4) : "")));
                     timer = delay.get();
                 } else {
                     timer--;
@@ -66,15 +75,19 @@ public class SpamBypass extends Module {
 
             case AmountPerTick -> {
                 for (int i = 0; i < amount.get(); i++) {
-                    mc.player.networkHandler.sendChatMessage(bc + " " + message);
+                    mc.player.networkHandler.sendChatMessage(((bypass.get() == Bypass.Command) ? bc.get() : "") + " " + message + (((bypass.get() == Bypass.RndString) ? " " + JinxUtils.randomString(4) : "")));
                 }
             }
-
         }
     }
 
     public enum Modes{
         DelayPerTick,
         AmountPerTick
+    }
+
+    public enum Bypass{
+        Command,
+        RndString
     }
 }
