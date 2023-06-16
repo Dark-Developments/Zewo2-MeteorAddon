8d7342b9e338b4f74c8216b3951ec766bffdc651
nxyi <Anthony.ashworth007@gmail.com>
2023-06-16 16:46:08 +0100

    added encryption

--- src/main/java/com/nxyi/addon/modules/Bookchat.java
+++ src/main/java/com/nxyi/addon/modules/Bookchat.java
@@ -20,6 +20,7 @@ import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.util.Hand;
 
 import java.lang.reflect.Array;
+import org.apache.commons.codec.binary.Base64;
 import java.util.*;
 
 public class Bookchat extends Module {
@@ -32,13 +33,21 @@ public class Bookchat extends Module {
     private final SettingGroup sgGeneral = this.settings.getDefaultGroup();
     private final Setting<String> person = sgGeneral.add(new StringSetting.Builder().name("Person").description("Person your chatting with").defaultValue("").build());
 
+    private final Setting<Boolean> encryption = sgGeneral.add(new BoolSetting.Builder()
+        .name("encryption")
+        .description("Encrypt the messages in base64.")
+        .defaultValue(false)
+        .build()
+    );
+
     @EventHandler
     private void onchat(SendMessageEvent event){
-        String msg = mc.player.getEntityName() + " : " + event.message;
+        String msg = event.message;
         if (mc.player.getMainHandStack().getItem().equals(Items.WRITABLE_BOOK)){
             event.cancel();
-            info(msg);
-            mc.player.networkHandler.sendPacket(new BookUpdateC2SPacket(mc.player.getInventory().selectedSlot, Collections.singletonList(msg), Optional.empty()));
+            info(mc.player.getEntityName() + " : " + msg);
+            String encrypted = Base64.encodeBase64String(msg.getBytes());
+            mc.player.networkHandler.sendPacket(new BookUpdateC2SPacket(mc.player.getInventory().selectedSlot, Collections.singletonList((encryption.get() ? encrypted : msg)), Optional.empty()));
         }
     }
 
@@ -56,6 +65,7 @@ public class Bookchat extends Module {
         PlayerEntity playerEntity = null;
 
         if (person.get().isEmpty()) return;
+        if(mc.world.getPlayers() == null) return;
         for (PlayerEntity p : mc.world.getPlayers()){
             if (p.getEntityName().equalsIgnoreCase(person.get())){
                 playerEntity = p;
@@ -65,16 +75,22 @@ public class Bookchat extends Module {
         if (playerEntity == null) return;
 
         if (playerEntity.getMainHandStack().getItem().equals(Items.WRITABLE_BOOK)){
+            if (playerEntity.getMainHandStack().getNbt().get("pages").toString() == null) return;
             String text = playerEntity.getMainHandStack().getNbt().get("pages").toString()
                 .replaceAll("\\]", "")
                 .replaceAll("\\[", "")
                 .replaceAll("\"", "")
                 .replaceAll("," , " ");
 
+            byte[] decodedBytes = Base64.decodeBase64(text);
+            String decodedString = new String(decodedBytes);
+
             if (Objects.equals(lasttext, text)) return;
 
+            boolean isBase64 = Base64.isBase64(text.getBytes());
+
             lasttext = text;
-            info(text);
+            info(playerEntity.getEntityName() + " : " + (isBase64 ? decodedString : text));
         }
     }
 }
