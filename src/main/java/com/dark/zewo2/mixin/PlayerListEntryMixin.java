7701de0043bc0aabd11af82c6170ad6c7b2d67ef
nxyi <anthony.ashworth007@gmail.com>
2023-11-12 22:08:17 +0000

    Boykisser fix

--- src/main/java/com/dark/zewo2/mixin/PlayerListEntryMixin.java
+++ src/main/java/com/dark/zewo2/mixin/PlayerListEntryMixin.java
@@ -5,29 +5,36 @@ import com.mojang.authlib.GameProfile;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.util.DefaultSkinHelper;
+import net.minecraft.client.util.SkinTextures;
 import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.function.Supplier;
+
 @Mixin(PlayerListEntry.class)
 public abstract class PlayerListEntryMixin {
+    @Final
     @Shadow
-    public abstract GameProfile getProfile();
+    private Supplier<SkinTextures> texturesSupplier;
 
     @Inject(method = "getSkinTextures", at = @At("HEAD"), cancellable = true)
-    private void onGetTexture(CallbackInfoReturnable<Identifier> info) {
+    private void onGetTexture(CallbackInfoReturnable<SkinTextures> cir) {
         if (Modules.get().get(Boykisser.class).isActive()) {
-            info.setReturnValue(Boykisser.boykisser);
+            cir.setReturnValue(
+                new SkinTextures(
+                    Boykisser.boykisser,
+                    texturesSupplier.get().textureUrl(),
+                    texturesSupplier.get().capeTexture(),
+                    texturesSupplier.get().elytraTexture(),
+                    SkinTextures.Model.SLIM,
+                    texturesSupplier.get().secure()
+                )
+            );
         }
     }
-
-//    @Inject(method = "", at = @At("HEAD"), cancellable = true)
-//    private void getModel(CallbackInfoReturnable<String> info) {
-//        if (Modules.get().get(Boykisser.class).isActive()) {
-//            info.setReturnValue("slim");
-//        }
-//    }
 }
