b73ff1033006e34abc4abf85a640b85d5cc44e55
nxyi <notrealemail@gmail.com>
2025-03-18 16:26:58 +0000

    fix shitty string logic in SoundCoordLogger

--- src/main/java/com/dark/zewo2/modules/SoundCoordLogger.java
+++ src/main/java/com/dark/zewo2/modules/SoundCoordLogger.java
@@ -34,18 +34,12 @@ public class SoundCoordLogger extends Module {
         }
     }
 
-    private String vectorToString(Vec3d vector, boolean... includeY) {
-        boolean reallyIncludeY = includeY.length <= 0 || includeY[0];
-        StringBuilder builder = new StringBuilder();
-        builder.append('(');
-        builder.append((int) Math.floor(vector.x));
-        builder.append(", ");
-        if(reallyIncludeY) {
-            builder.append((int) Math.floor(vector.y));
-            builder.append(", ");
-        }
-        builder.append((int) Math.floor(vector.z));
-        builder.append(")");
-        return builder.toString();
+    private String vectorToString(Vec3d vector) {
+        return String.format(
+            "(%s, %s, %s)",
+            Math.floor(vector.x),
+            Math.floor(vector.y),
+            Math.floor(vector.z)
+        );
     }
 }
